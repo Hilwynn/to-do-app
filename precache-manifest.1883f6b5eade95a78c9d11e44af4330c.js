aefa2ff3b9b0afdc3aa922c4d9b7b0c2ba28b64b
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-23 14:52:31 +0200

    Updates

---
+++ precache-manifest.1883f6b5eade95a78c9d11e44af4330c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60e6816a297c28fee7c8",
+    "url": "/to-do-app/static/js/runtime~main.60e6816a.js"
+  },
+  {
+    "revision": "46c35911427afa6d4de8",
+    "url": "/to-do-app/static/js/main.46c35911.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/to-do-app/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "46c35911427afa6d4de8",
+    "url": "/to-do-app/static/css/main.48a3b489.chunk.css"
+  },
+  {
+    "revision": "4dcb30942162a1c8134a1c45677db501",
+    "url": "/to-do-app/index.html"
+  }
+];
